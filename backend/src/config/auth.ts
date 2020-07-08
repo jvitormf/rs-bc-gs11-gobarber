56fbc22b11afd279bc58ea26acf3610cf36904e4
João Vitor Ferreira <j.vitor.mf@gmail.com>
2020-07-08 18:05:40 -0300

    feat: add production services

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: 'a4b38b32653a2e404cb995127fac1442',
+    secret: process.env.APP_SECRET,
     expiresIn: '1d',
   },
 };
--- backend/src/modules/users/infra/http/controllers/SessionsController.ts
+++ backend/src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import SessionUserService from '@modules/users/services/SessionUserService';
 
@@ -14,8 +15,6 @@ export default class SesssionsController {
       password,
     });
 
-    delete user.password;
-
-    return response.json({ user, token });
+    return response.json({ user: classToClass(user), token });
   }
 }
