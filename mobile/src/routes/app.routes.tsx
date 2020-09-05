c8cb4f179607acd0c037238cd5d038eb9578719c
Johnny <johnny@johnnys-air.home>
2020-09-05 01:48:24 -0300

    fix: fix android files and run pod install

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -16,7 +16,6 @@ const AppRoutes: React.FC = () => (
     }}
   >
     <App.Screen name="Dashboard" component={Dashboard} />
-    <App.Screen name="Profile" component={Profile} />
     <App.Screen name="CreateAppointment" component={CreateAppointment} />
     <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
 
