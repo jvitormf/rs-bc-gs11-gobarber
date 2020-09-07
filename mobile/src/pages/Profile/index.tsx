60209bcbb14357fcee1cde71bf4e94a5ef3d68b6
Johnny <johnny@johnnys-air.home>
2020-09-07 00:08:55 -0300

    feat: add profile screen

--- mobile/src/pages/Profile/index.tsx
+++ mobile/src/pages/Profile/index.tsx
@@ -1,11 +1,221 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback, useRef } from 'react';
+import {
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  Alert,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import {
+  Container,
+  BackButton,
+  Title,
+  UserAvatarButton,
+  UserAvatar,
+} from './styles';
+
+interface ProfileFormData {
+  name: string;
+  email: string;
+  current_password: string;
+  password: string;
+  password_confirmation: string;
+}
 
 const Profile: React.FC = () => {
+  const { user, updateUser } = useAuth();
+  const formRef = useRef<FormHandles>(null);
+  const { goBack } = useNavigation();
+
+  const emailInputref = useRef<TextInput>(null);
+  const currentPasswordInputref = useRef<TextInput>(null);
+  const passwordInputref = useRef<TextInput>(null);
+  const passwordConfirmationInputref = useRef<TextInput>(null);
+
+  const handleProfile = useCallback(
+    async (data: ProfileFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome é obrigatório!'),
+          email: Yup.string()
+            .email('Digite um e-mail válido!')
+            .required('E-mail é obrigatório!'),
+          current_password: Yup.string(),
+          password: Yup.string().when('current_password', {
+            is: (val) => !!val.length,
+            then: Yup.string().required('Campo obrigatório!'),
+            otherwise: Yup.string(),
+          }),
+          password_confirmation: Yup.string()
+            .when('current_password', {
+              is: (val) => !!val.length,
+              then: Yup.string().required('Campo obrigatório!'),
+              otherwise: Yup.string(),
+            })
+            .oneOf([Yup.ref('password'), null], 'Confirmação incorreta'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        const {
+          name,
+          email,
+          password,
+          password_confirmation,
+          current_password,
+        } = data;
+
+        const formData = {
+          name,
+          email,
+          ...(current_password
+            ? {
+                current_password,
+                password,
+                password_confirmation,
+              }
+            : {}),
+        };
+
+        const response = await api.put('profile', formData);
+
+        updateUser(response.data);
+
+        Alert.alert('Perfil atualizado com sucesso');
+
+        goBack();
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        Alert.alert(
+          'Erro na atualização do perfil',
+          'Ocorreu um erro na atualização do perfil, tente novamente!',
+        );
+      }
+    },
+    [goBack, updateUser],
+  );
+
+  const handleGoBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
   return (
-    <View>
-      <View />
-    </View>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <BackButton onPress={handleGoBack}>
+              <Icon name="chevron-left" size={24} color="#999591" />
+            </BackButton>
+            <UserAvatarButton onPress={() => {}}>
+              <UserAvatar source={{ uri: user.avatar_url }} />
+            </UserAvatarButton>
+            <View>
+              <Title>Meu perfil</Title>
+            </View>
+
+            <Form initialData={user} ref={formRef} onSubmit={handleProfile}>
+              <Input
+                name="name"
+                icon="user"
+                placeholder="Nome completo"
+                autoCapitalize="words"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  emailInputref.current?.focus();
+                }}
+              />
+
+              <Input
+                ref={emailInputref}
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCorrect={false}
+                autoCapitalize="none"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  currentPasswordInputref.current?.focus();
+                }}
+              />
+
+              <Input
+                ref={currentPasswordInputref}
+                name="current_password"
+                icon="lock"
+                placeholder="Senha"
+                secureTextEntry
+                textContentType="newPassword"
+                returnKeyType="next"
+                containerStyle={{ marginTop: 16 }}
+                onSubmitEditing={() => passwordInputref.current?.focus()}
+              />
+
+              <Input
+                ref={passwordInputref}
+                name="password"
+                icon="lock"
+                placeholder="Nova senha"
+                secureTextEntry
+                textContentType="newPassword"
+                returnKeyType="next"
+                onSubmitEditing={() =>
+                  passwordConfirmationInputref.current?.focus()}
+              />
+
+              <Input
+                ref={passwordConfirmationInputref}
+                name="password_confirmation"
+                icon="lock"
+                placeholder="Confirmar senha"
+                secureTextEntry
+                textContentType="newPassword"
+                returnKeyType="send"
+                onSubmitEditing={() => formRef.current?.submitForm()}
+              />
+
+              <Button onPress={() => formRef.current?.submitForm()}>
+                Cofirmar mudanças
+              </Button>
+            </Form>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </>
   );
 };
 
