ea44c9c703080d8a64de20e109bd09c5ef639e57
Johnny <johnny@johnnys-air.home>
2020-09-06 21:08:41 -0300

    feat: add appointment created confirmation screen

--- mobile/src/pages/AppointmentCreated/index.tsx
+++ mobile/src/pages/AppointmentCreated/index.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useNavigation } from '@react-navigation/native';
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
 
 const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
+
   return (
-    <View>
-      <View />
-    </View>
+    <Container>
+      <Icon name="check" size={80} color="#04d631" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>Qualquer coisa</Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
   );
 };
 
